265a6be3f74b9a0eb30681ac6d7abcab4b5f46b2
Johan García <jaggandreyj1@gmail.com>
2024-04-21 04:07:38 -0500

    Implementation of "Repository" classes

---
+++ src/Services/FoodShopService/FoodShop.Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodShop.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
+        T GetById(int id);
+        Task<T> GetByIdAsync(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ src/Services/FoodShopService/FoodShop.Repository/Repository.cs
@@ -0,0 +1,53 @@
+﻿using FoodShop.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodShop.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly FoodShopDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(FoodShopDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
