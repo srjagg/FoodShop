234457333b123e043a3602efc61639295b76d79c
Johan García <jaggandreyj1@gmail.com>
2024-04-21 04:07:09 -0500

    Implementation of UnitOfWork design pattern

---
+++ src/Services/FoodShopService/FoodShop.UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using FoodShop.Model.Models;
+using FoodShop.Repository;
+
+namespace FoodShop.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IRepository<User> UserRepository { get; }
+        IRepository<Food> FoodRepository { get; }
+        IRepository<Order> OrderRepository { get; }
+        IRepository<OrderDetail> OrderDetailRepository { get; }
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ src/Services/FoodShopService/FoodShop.UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using FoodShop.Model.Models;
+using FoodShop.Persistence;
+using FoodShop.Repository;
+
+namespace FoodShop.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly FoodShopDbContext _context;
+        public UnitOfWork(FoodShopDbContext context) 
+        {
+            _context = context;
+            UserRepository = new Repository<User>(_context);
+            FoodRepository = new Repository<Food>(_context);
+            OrderRepository = new Repository<Order>(_context);
+            OrderDetailRepository = new Repository<OrderDetail>(_context);
+        }
+
+        public IRepository<User> UserRepository { get; private set; }
+        public IRepository<Food> FoodRepository { get; private set; }
+        public IRepository<Order> OrderRepository { get; private set; }
+        public IRepository<OrderDetail> OrderDetailRepository { get; private set; }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
