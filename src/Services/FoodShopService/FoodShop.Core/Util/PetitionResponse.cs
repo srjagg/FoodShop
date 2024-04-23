afa29ec64faa75228033216375def9a38acaf276
Johan García <jaggandreyj1@gmail.com>
2024-04-22 19:31:28 -0500

    Generic PetitionResponse class is created

---
+++ src/Services/FoodShopService/FoodShop.Core/Util/PetitionResponse.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShop.Core.Util
+{
+    public class PetitionResponse<T>
+    {
+        public bool? Success { get; set; }
+        public string? Message { get; set; }
+        public string? Module { get; set; }
+        public string? URL { get; set; }
+        public T? Result { get; set; }
+
+        public PetitionResponse() {}
+
+        public PetitionResponse(bool success, string message, string module, string url, T result)
+        {
+            Success = success;
+            Message = message;
+            Module = module;
+            URL = url;
+            Result = result;
+        }
+    }
+}
