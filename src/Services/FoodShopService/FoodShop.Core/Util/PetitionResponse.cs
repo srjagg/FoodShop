dd0ee23d6420931e03c229db2bab4e7df8fc0031
Johan García <jaggandreyj1@gmail.com>
2024-05-08 02:14:28 -0500

    Comments are added to "PetitionResponse" structure

--- src/Services/FoodShopService/FoodShop.Core/Util/PetitionResponse.cs
+++ src/Services/FoodShopService/FoodShop.Core/Util/PetitionResponse.cs
@@ -1,11 +1,30 @@
 ﻿namespace FoodShop.Core.Util
 {
+    /// <summary>
+    /// Representa la respuesta de una petición.
+    /// </summary>
+    /// <typeparam name="T">El tipo de resultado de la petición.</typeparam>
     public class PetitionResponse<T>
     {
+        /// <summary>
+        /// Indica si la petición fue exitosa.
+        /// </summary>
         public bool Success { get; set; }
+        /// <summary>
+        /// El mensaje asociado con la petición.
+        /// </summary>
         public string? Message { get; set; }
+        /// <summary>
+        /// El módulo o componente que emitió la petición.
+        /// </summary>
         public string? Module { get; set; }
+        /// <summary>
+        /// La URL asociada con la petición (si corresponde).
+        /// </summary>
         public string? URL { get; set; }
+        /// <summary>
+        /// El resultado de la petición.
+        /// </summary>
         public T? Result { get; set; }
 
         public PetitionResponse() {}
